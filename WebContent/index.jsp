05ff96be7699f7b75fe1d555ce87db03d7bf8a8c
Stick Cruz <stick.cruz1992@gmail.com>
2018-10-02 22:16:03 -0500

    simple hola mundo con servlet

---
+++ WebContent/index.jsp
@@ -0,0 +1,21 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+	pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Hola Mundo</title>
+</head>
+<body>
+	<h1>Hola Mundo Stick</h1>
+
+	<form action="login" method="post">
+		Nombre: <input type="text" name="nombre" /> 
+		<br><br>
+		Contraseña: <input type="password" name="contrasena" /> 
+		<br><br>
+		<input type="submit" name="ingresar" value="Ingresar" />
+	</form>
+
+</body>
+</html>
---
+++ src/com/holamundo/beans/LoginBean.java
@@ -0,0 +1,42 @@
+package com.holamundo.beans;
+
+import java.io.Serializable;
+
+public class LoginBean implements Serializable{
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private String nombre;
+	private String contrasena;
+	
+	public LoginBean() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public LoginBean(String nombre, String contrasena) {
+		super();
+		this.nombre = nombre;
+		this.contrasena = contrasena;
+	}
+	
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getContrasena() {
+		return contrasena;
+	}
+	public void setContrasena(String contrasena) {
+		this.contrasena = contrasena;
+	}
+	
+	public boolean Autenticacion() {
+		return contrasena.equals("holi")?true:false;
+	}
+
+}
